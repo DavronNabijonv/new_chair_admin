5e270ad2d3c8d02aae87584e55f2a31d01fce92c
Davron Chetin <nabijonovdavronbek619@gmail.com>
2024-09-02 03:23:58 +0500

    login_page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [tog, setTog] = useState(false);
+  const [user_info, setUser_info] = useState({
+    fullname: "",
+    username: "",
+    pasword: "",
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="togle_log">
+        {tog ? (
+          <div className="royhat">
+            <input type="text" placeholder="To`liq ismingizni kiriting" />
+            <input type="text" placeholder="Foydalanuvchi nomi" />
+            <input type="text" placeholder="Parolni kiriting" />
+            <button>Ro`yhatdan o`tish</button>
+          </div>
+        ) : (
+          <div className="royhat">
+            <input type="text" placeholder="Foydalanuvchi nomini kiriting" />
+            <input type="text" placeholder="Parolni kiriting" />
+            <button>Kirish</button>
+          </div>
+        )}
+        {tog ? (
+          <p className="change_txt">
+            Akkaountga -- <span onClick={() => {setTog(false);}}>Kirish</span>
+          </p>
+        ) : (
+          <p className="change_txt">
+            Akkaunt -- <span onClick={() => { setTog(true);}}>yartaish</span>
+          </p>
+        )}
+      </div>
   );
 }
 
