ee60510334989af1d16cc74079dafdfc92fbe88f
Davron Chetin <nabijonovdavronbek619@gmail.com>
2024-09-13 19:48:13 +0500

    server_bug

--- src/barcha_sorovlar/post/auth_post.js
+++ src/barcha_sorovlar/post/auth_post.js
@@ -5,7 +5,7 @@ const apiUrl = process.env.REACT_APP_API_BASE_URL;
 export async function create_Admin(endpoint, info) {
     try {
         const response = await axios.post(`${apiUrl}/${endpoint}`, info);
-        return { response, success: true };  // Return an object with response and success flag
+        return { response, success: true };  // Return  oan object with response and success flag
     } catch (error) {
         return {
             response: error.response || { data: { message: "Unknown error occurred" } },
