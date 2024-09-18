ce797659a2876ea51e5628fb112d3890b5fa5c78
Davron Chetin <nabijonovdavronbek619@gmail.com>
2024-09-18 09:29:38 +0500

    delete_update

--- src/barcha_sorovlar/put_patch_del/deleteFurniture.js
+++ src/barcha_sorovlar/put_patch_del/deleteFurniture.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function delete_furniture(furniture_id, del_token) {
   const apiUrl = process.env.REACT_APP_API_BASE_URL;
-  const url = `${apiUrl}/v1/api/products/${furniture_id}`; // Use furniture_id in the URL
+  const url = `${apiUrl}/products/${furniture_id}`; // Use furniture_id in the URL
 
   try {
     const response = await axios.delete(url, {
