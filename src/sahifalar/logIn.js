6165f2098f48d4c8d4dbf08fa7668f60a500cebd
Davron Chetin <nabijonovdavronbek619@gmail.com>
2024-09-18 09:10:59 +0500

    update_request

--- src/sahifalar/logIn.js
+++ src/sahifalar/logIn.js
@@ -7,7 +7,7 @@ import Error_res from "./suc_err/error";
 
 function LogIn() {
   const create_url = `/auth/add`;  // Relative ppath
-  const logIn_url = `/auth/login`; // Relative pathhh
+  const logIn_url = `/auth/login`; // Relative path
 
   const { error_response, setError_response } = useContext(ShowError);
   const { setRes_message } = useContext(ResponseMessage);
